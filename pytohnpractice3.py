cd8184ebeee02c21d85cd8a6eaf394ce2cb4c439
SARTHAK SINGH CHAUHAN <77792367+sartronus@users.noreply.github.com>
2024-04-13 18:52:14 +0530

    Add files via upload

---
+++ pytohnpractice3.py
@@ -0,0 +1,66 @@
+#problems from https://www.practicepython.org/
+# character input problem:
+#name=input("please enter your name: ")
+#age=int(input("please enter your age: "))
+#quality=input("please enter your best quality: ")
+
+#fin=(f"hello {name}. Your age is {age} and your best quality is {quality}")
+#print(fin)
+#___________________________________________________________________________________________________
+#odd or even problem:
+#try:
+#    def odd_eve(a):
+#        if a % 2 == 0:
+#            print("even")
+#        else:
+#            print("odd")
+#    num=int(input("enetr num to check if its even or odd: "))
+#    odd_eve(num)
+#except ValueError:
+#    print("only numbers can be determined as odd or even")
+#___________________________________________________________________________________________________________
+#List Less Than Ten problem:
+#list=[]
+#num=0
+#while num < 9:
+#    num += 1
+#
+#    list.append(num)
+#print(list)
+#__________________________________________________________________________________________________
+#Divisors problem
+#num=int(input("enter number of which you want to check divisor: "))
+#extend=int(input("enter number till which you want to find the divisor: "))
+
+#list=[]
+#for i in range(1,extend):
+#    if i % num == 0:
+#        list.append(i)
+#print(list)
+#________________________________________________________________________________________________________
+#List Overlap problem
+#list1=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#list2=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#uniqlist=[]
+#uniqlist2=[]
+#uniqlist3=[]
+
+#for i  in list1:
+#    if i not in uniqlist:
+#        uniqlist.append(i)
+#u=uniqlist
+#print(u)
+#for i in list2:
+#    if i not in uniqlist2:
+#        uniqlist2.append(i)
+#u2=uniqlist2
+#print(u2)
+#for i in u:
+#    if i in u2:
+#        uniqlist3.append(i)
+#print(uniqlist3)
+
+
+
+
+
